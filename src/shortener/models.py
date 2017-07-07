8d54b2f1672649a1b5ed16059f9cdd697467a2da
nwre87 <nwre87@gmail.com>
2017-07-06 21:40:00 -1000

    add shortcode column in KirrURL model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class KirrURL(models.Model):
   url = models.CharField(max_length=220, )
+  shortcode = models.CharField(max_length=15, unique=True)
 
   def __str__(self):
     return str(self.url)
