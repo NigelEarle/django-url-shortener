2b8cd68f395cef0c1a3f543a47587f513a64ad22
nwre87 <nwre87@gmail.com>
2017-07-08 12:12:49 -1000

    add command management

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -9,11 +9,14 @@ class KirrURLManager(models.Manager):
     qs = qs_main.filter(active=True)
     return qs
 
-  def refresh_shortcodes(self):
+  def refresh_shortcodes(self, items=100):
     qs = KirrURL.objects.filter(id__gte=1)
+    if items is not None and isinstance(items, int):
+      qs = qs.order_by('id')[:items]
     new_codes = 0
     for q in qs:
       q.shortcode = create_shortcode(q)
+      print(q.id)
       q.save()
       new_codes += 1
 
