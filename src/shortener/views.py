e01d7af83a645f7efa5dc38c028a20d884a3e477
nwre87 <nwre87@gmail.com>
2017-07-08 12:26:24 -1000

    add function and class based views, routes

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def kirr_redirect_view(request, *args, **kwargs): #function based view
+  return HttpResponse("hello")
+
+class KirrCBView(View):
+  def get(self, reques, *args, **kwargs): #class based view
+    return HttpResponse("hello again")
