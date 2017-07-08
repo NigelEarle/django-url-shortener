8fd07aaf25c56718da78dd8ce9246b3fba5919bb
nwre87 <nwre87@gmail.com>
2017-07-07 14:13:52 -1000

    create refresh_shortcodes model manager

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,11 +3,30 @@ from .utils import code_generator, create_shortcode
 
 # Create your models here.
 
+class KirrURLManager(models.Manager):
+  def all(self, *args, **kwargs):
+    qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+    qs = qs_main.filter(active=True)
+    return qs
+
+  def refresh_shortcodes(self):
+    qs = KirrURL.objects.filter(id__gte=1)
+    new_codes = 0
+    for q in qs:
+      q.shortcode = create_shortcode(q)
+      q.save()
+      new_codes += 1
+
+    return "New codes made: {i}".format(i=new_codes)
+
 class KirrURL(models.Model):
   url = models.CharField(max_length=220, )
   shortcode = models.CharField(max_length=15, unique=True, blank=True)
   updated = models.DateTimeField(auto_now=True)
   timestamp = models.DateTimeField(auto_now_add=True)
+  active = models.BooleanField(default=True)
+
+  objects = KirrURLManager()
 
   def save(self, *args, **kwargs):
     if self.shortcode is None or self.shortcode == "":
