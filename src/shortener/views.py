fc1e98a45ef7e6270b76014ff9e24b67e35bbb13
nwre87 <nwre87@gmail.com>
2017-07-09 23:12:08 -1000

    add view func

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -2,9 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from .models import KirrURL
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #function based view
-  return HttpResponse("hello {sc}".format(sc=shortcode))
+  obj_url = None
+  qs = KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
+  if qs.exists() and qs.count() == 1:
+    obj = qs.first()
+    obj_url = obj.url
+
+  return HttpResponse("hello {sc}".format(sc=obj_url))
 
 class KirrCBView(View):
   def get(self, request ,shortcode=None, *args, **kwargs): #class based view
