6a601b608050a11c00be8aaab9475e646da4db01
nwre87 <nwre87@gmail.com>
2017-07-14 16:50:45 -1000

    add shortcode length

--- src/kirr/urls.py
+++ src/kirr/urls.py
@@ -19,6 +19,6 @@ from shortener.views import kirr_redirect_view, KirrCBView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^a/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
-    url(r'^b/(?P<shortcode>[\w-]+)/$', KirrCBView.as_view()),
+    url(r'^a/(?P<shortcode>[\w-]+){6,15}/$', kirr_redirect_view),
+    url(r'^b/(?P<shortcode>[\w-]+){6,15}/$', KirrCBView.as_view()),
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,26 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from .models import KirrURL
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #function based view
-  
   obj = get_object_or_404(KirrURL, shortcode=shortcode)
-  
-  # NOTES
-  
-  # obj_url = obj.url
-  # obj_url = None
-  # qs = KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
-  # if qs.exists() and qs.count() == 1:
-  #   obj = qs.first()
-  #   obj_url = obj.url
-
-  return HttpResponse("hello {sc}".format(sc=obj.url))
+  return HttpResponseRedirect(obj.url)
 
 class KirrCBView(View):
   def get(self, request ,shortcode=None, *args, **kwargs): #class based view
     obj = get_object_or_404(KirrURL, shortcode=shortcode)
-    return HttpResponse("hello again {sc}".format(sc=shortcode))
+    return  HttpResponseRedirect(obj.url)
     
+  def post(self, request, *args, **kwargs):
+    return HttpResponse()
