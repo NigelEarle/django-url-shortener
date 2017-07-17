62a01677b0484f1b20d3114074b0fb5f29616895
nwre87 <nwre87@gmail.com>
2017-07-17 13:20:47 -1000

    add form class

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -2,16 +2,29 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrURL
 # Create your views here.
 
 class HomeView(View):
   def get(self, request, *args, **kwargs):
-    return render(request, "shortener/home.html", {})
+    the_form = SubmitUrlForm()
+    context = {
+      "title": "Submit URL",
+      "form": the_form
+    }
+    return render(request, "shortener/home.html", context)
 
   def post(self, request, *args, **kwargs):
-    print(request.POST.get("url"))
-    return render(request, "shortener/home.html", {})
+    form = SubmitUrlForm(request.POST)
+    if form.is_valid():
+      print(form.cleaned_data)
+
+    context = {
+      "title": "Submit URL",
+      "form": form
+    }
+    return render(request, "shortener/home.html", context)
 
 class KirrCBView(View):
   def get(self, request ,shortcode=None, *args, **kwargs): #class based view
