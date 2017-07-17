94756a85341491d2479abf3b37cee83df9a6562c
nwre87 <nwre87@gmail.com>
2017-07-17 12:35:37 -1000

    add templates

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -4,14 +4,12 @@ from django.views import View
 
 from .models import KirrURL
 # Create your views here.
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs): #function based view
-  obj = get_object_or_404(KirrURL, shortcode=shortcode)
-  return HttpResponseRedirect(obj.url)
+
+class HomeView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, "shortener/home.html", {})
 
 class KirrCBView(View):
   def get(self, request ,shortcode=None, *args, **kwargs): #class based view
     obj = get_object_or_404(KirrURL, shortcode=shortcode)
     return  HttpResponseRedirect(obj.url)
-    
-  def post(self, request, *args, **kwargs):
-    return HttpResponse()
